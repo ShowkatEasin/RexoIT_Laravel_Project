bb81711dfffc6e47b3d5682d492f61533bf8e945
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-11-26 19:02:41 +0600

    Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,6 +34,13 @@
 
                         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14607.663588536767!2d90.4393024!3d23.75037835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1669467118039!5m2!1sen!2sbd" width="600" height="450" style="border:0;" 
                         allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                        <script>
+                            function showPosition(position) {
+                              x.innerHTML = "Latitude: " + position.coords.latitude +
+                              "<br>Longitude: " + position.coords.longitude;
+                            }
+                        </script>
   
                             </div>
                           </div>
