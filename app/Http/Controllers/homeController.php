831464c346c3d1d2328fcf4be5bb07f054509a1e
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-11-26 13:44:31 +0600

    AddIP

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stevebauman\Location\Facades\Location;
+class homeController extends Controller
+{
+    public function index(Request $request){
+        
+       // $ip = request()->ip();  //Dynamic
+          $ip = '103.200.38.143'; //Static
+          $data ='Location'::get($ip);
+          //dd($data);
+        return view('welcome',compact('data'));
+    }
+}
---
+++ resources/views/backend/category/create.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Create Category</title>
+  </head>
+  <body>
+    
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Create Category</h1>
+                <form action="">
+                    <div class="form-group">
+                        <label for="name">Category Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Category Name" >
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Category Description</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Category Name" >
+                    </div>
+                    <div class="form-group">
+                        <input type="Submit" class="btn btn-success" value="Submit" >
+                    </div>
+
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
